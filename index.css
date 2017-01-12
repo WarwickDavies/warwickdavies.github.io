28ac6b052ef8cd8413ab2fabed06c3e59a50249b
WarwickDavies <5299@pierson.nl>
2017-01-12 15:48:15 +0100

    Create index.css

---
+++ index.css
@@ -0,0 +1,44 @@
+h1 {
+   text-align: center;
+   font-size: 70px;
+   color: darkblue;
+   font-family: fantasy;
+   letter-spacing: 2px;
+   }
+p1  {
+   color: black;
+   font-family: fantasy;
+   font-size: px;
+   font-size: 20px;
+   padding-top: 60px;
+   text-align: center;
+   }
+a {
+  color: red;
+  font-family: fantasy;
+  font-size: 34px;
+  padding-top: 12px;
+}
+
+ul {
+   color: darkblue;
+   font-family: darkblue;
+   line-height: 26px;
+   }
+body {
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-color: blue;
+    background-size: 100%;
+    height: 100vh;
+    width: 100vw;
+}
+
+
+h2 {
+  text-align: inherit;
+  margin: auto;
+  font-size: 12px;
+  color: crimson;
+}
